cecf53c906a92fc4e675f202720051f7f3993977
Kotobotov.ru <dima@kotobotov.ru>
2018-10-17 23:25:17 +0700

    :bug:  add Genome Sequencing puzzle
    Took 1 minute

---
+++ src/main/scala/Genome_Sequencing/Solution.scala
@@ -0,0 +1,53 @@
+/**
+  * Created by Kotobotov.ru on 17.10.2018.
+  */
+import scala.annotation.tailrec
+import scala.collection.mutable
+
+object Solution extends App {
+  val n = readInt - 1
+  val genoms = (0 to n).permutations.toList
+
+  val genomCode = for (i <- (0 to n)) yield readLine
+//val genomCode = Seq("BAG", "GAA", "TACAGA")
+
+  case class MerjedWords(left: String, right: String)
+
+  val memoize = mutable.HashMap.empty[MerjedWords, Int]
+
+  def mergeByIndex(left: Int, right: Int): Int = {
+    def calculateMinMergeSize(wordsPair: MerjedWords): Int = {
+      @tailrec
+      def helper(in: List[Char], target: List[Char], result: Int): Int = {
+        val result = if ((in zip target).forall {
+                           case (left, right) => left == right
+                         }) in.size
+        else 0
+
+        in match {
+          case Nil               => result
+          case _ if (result > 0) => result
+          case _ :: tail         => helper(tail, target, result)
+        }
+      }
+      helper(wordsPair.left.toList, wordsPair.right.toList, 0)
+    }
+
+    val wordsPair = MerjedWords(genomCode(left), genomCode(right))
+    if (memoize.contains(wordsPair)) memoize(wordsPair)
+    else {
+      val minMerjedSize = calculateMinMergeSize(wordsPair)
+      memoize += wordsPair -> minMerjedSize
+      minMerjedSize
+    }
+  }
+
+  val maxMergedParts = genoms
+    .map(_.sliding(2).collect {
+      case input: Vector[Int] => mergeByIndex(input(0), input(1))
+    }.sum)
+    .max
+
+  println(genomCode.map(_.length).sum - maxMergedParts)
+
+}

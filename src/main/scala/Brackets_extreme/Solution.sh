e7d2b7670f27bacd0fb7bd44647e497df5b9fa35
Kotobotov <dima@kotobotov.ru>
2022-08-08 23:31:14 +0300

    add bash "Brackets" solution (not complite?)

--- src/main/scala/Brackets_extreme/Solution.sh
+++ src/main/scala/Brackets_extreme/Solution.sh
@@ -1,3 +1,29 @@
-read -r expression
+declare -A ClosingPartForBracet
+ClosingPartForBracet[\{]="}"
+ClosingPartForBracet[\(]=")"
+ClosingPartForBracet[\<]=">"
+ClosingPartForBracet[\[]="]"
 
-echo "true/false"
+
+function helper(){
+local head=${1:0:1}
+local tail=${1:1}
+local stack=$2
+
+case $head in
+"")
+    echo $stack EMPTY
+    ;;
+\{|\<|\[\).*)
+    echo $(helper $tail $head$stack)
+    ;;
+\}|\>|\]\).*)
+    [[ ${ClosingPartForBracet[${stack:0:1}]}==$head ]] && echo $(helper $tail ${stack:1}) || echo "false"
+    ;;
+*)
+    echo $(helper $tail $stack)
+    ;;
+esac
+}
+
+[[ $(helper $1 "")=='' ]] && echo 'true' || echo 'false'

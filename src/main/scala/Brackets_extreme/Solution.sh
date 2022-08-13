d241c127ab4dadf6674e6f175f9a81f14b9e5c8b
Kotobotov <dima@kotobotov.ru>
2022-08-13 16:31:56 +0300

    refactor bash solution of Brackets

--- src/main/scala/Brackets_extreme/Solution.sh
+++ src/main/scala/Brackets_extreme/Solution.sh
@@ -1,30 +1,34 @@
+#classic tail recursive solution which is the same on any languages (without any fansy staff)
+function process(){
 declare -A ClosingPartForBracet
 ClosingPartForBracet[\{]="}"
 ClosingPartForBracet[\(]=")"
 ClosingPartForBracet[\<]=">"
 ClosingPartForBracet[\[]="]"
 
-read -r expression
-
 function helper(){
-local head=${1:0:1}
-local tail=${1:1}
-local stack=$2
+    local head=${1:0:1}
+    local tail=${1:1}
+    local stack=$2
 
-case $head in
-"")
-    echo $stack
-    ;;
-[\[\(\{\(\<])
-    echo $(helper "$tail" "$head$stack")
-    ;;
-[\]\)\}\)\>])
-   [[ "$stack" != "" && "${ClosingPartForBracet["${stack:0:1}"]}" = $head ]] && echo $(helper "$tail" "${stack:1}") || echo "false"
-    ;;
-*)
-    echo $(helper "$tail" "$stack")
-    ;;
-esac
+    case $head in
+    "")
+        echo $stack
+        ;;
+    [\[\(\{\(\<])
+        echo $(helper "$tail" "$head$stack")
+        ;;
+    [\]\)\}\)\>])
+        [[ ! -z "$stack" && ${ClosingPartForBracet["${stack:0:1}"]} = $head ]] && echo $(helper "$tail" "${stack:1}") || echo "TerminateWithNonEmtpy"
+        ;;
+    *)
+        echo $(helper "$tail" "$stack")
+        ;;
+    esac
+    }
+
+[[ $(helper "$1" "") == "" ]] && echo 'true' || echo 'false'
 }
 
-[[ $(helper $expression "") == "" ]] && echo 'true' || echo 'false'
+read -r expression
+echo $(process "$expression")
